761b266f620148ac7640edb9d73b2b61d619c3c1
Sahan Takhachhen <takhachhensahan@gmail.com>
2025-12-28 12:43:09 +0545

    securing smtp data in dot environment file

--- project/settings.py
+++ project/settings.py
@@ -144,10 +144,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
 }
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 't02576148@gmail.com'
-EMAIL_HOST_PASSWORD = 'uprddspejohlggop'
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND' )
+EMAIL_HOST = os.getenv('EMAIL_HOST' )
+EMAIL_PORT = os.getenv('EMAIL_PORT' )
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS' ) 
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER' )
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD' )
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
